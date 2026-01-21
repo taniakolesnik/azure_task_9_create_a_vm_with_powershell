b500ab3c748de061907da002fea4a9c30209dd61
Tania Kolesnik <kolesnik@gmail.com>
2026-01-21 15:00:11 +0000

    add solution

--- scripts/validate-artifacts.ps1
+++ scripts/validate-artifacts.ps1
@@ -195,7 +195,7 @@ if ($virtualMachine.properties.storageProfile.imageReference.offer.Contains('ubu
     throw "Virtual Machine uses wrong OS image. Please make sure that your script creates a VM from image with friendly name 'Ubuntu2204' and try again." 
 }
 
-if ($virtualMachine.properties.hardwareProfile.vmSize -eq "Standard_B1s") { 
+if ($virtualMachine.properties.hardwareProfile.vmSize -eq "Standard_B2ats_v2") { 
     Write-Output "`u{2705} Checked Virtual Machine size - OK"
 } else { 
     Write-Output `u{1F914}
--- task.ps1
+++ task.ps1
@@ -1,4 +1,4 @@
-$location = "uksouth"
+$location = "ukwest"
 $resourceGroupName = "mate-azure-task-9"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
@@ -6,11 +6,12 @@ $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $publicIpAddressName = "linuxboxpip"
+$dnsPrefix = "matebox-ukwest-0001"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
+$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa_azure.pub" -Raw
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
-$vmSize = "Standard_B1s"
+$vmSize = "Standard_B2ats_v2"
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
@@ -20,4 +21,40 @@ $nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Directio
 $nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
 New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
 
-# ↓↓↓ Write your code here ↓↓↓
+Write-Host "Creating a virtual network $virtualNetworkName and subnet $subnetName ..."
+$subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
+New-AzVirtualNetwork `
+  -Name $virtualNetworkName `
+  -ResourceGroupName $resourceGroupName -Location $location `
+  -AddressPrefix $vnetAddressPrefix `
+  -Subnet $subnet
+
+Write-Host "Creating a public IP address $publicIpAddressName ..."
+New-AzPublicIpAddress -Name $publicIpAddressName `
+  -ResourceGroupName $resourceGroupName `
+  -Sku Standard `
+  -AllocationMethod Static `
+  -DomainNameLabel $dnsPrefix `
+  -Location $location
+
+Write-Host "Creating a public SSH key $sshKeyName ..."
+New-AzSshKey `
+  -ResourceGroupName $resourceGroupName `
+  -Name $sshKeyName `
+  -PublicKey $sshKeyPublicKey
+
+Write-Host "Creating a virtual machine $vmName ..."
+New-AzVM `
+  -Name $vmName `
+  -ResourceGroupName $resourceGroupName `
+  -Location $location `
+  -VirtualNetworkName $virtualNetworkName `
+  -SubnetName $subnetName `
+  -PublicIpAddressName $publicIpAddressName `
+  -SecurityGroupName $networkSecurityGroupName `
+  -SshKeyName $sshKeyName `
+  -Image $vmImage `
+  -Size $vmSize
+
+
+
